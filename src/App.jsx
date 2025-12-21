9af3e833925c962ba1f2cea9bb0330a00ee172d1
Tariful Hasan <tarif305@gmail.com>
2025-12-22 00:20:04 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>smart deal client side</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/routes/Routes.jsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from "react-router";
+import RootLayout from "../Layouts/RootLayout";
+import Home from "../components/Home/Home";
+import AllProducts from "../components/AllProducts/AllProducts";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: RootLayout,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+      {
+        path: "allProducts",
+        Component: AllProducts,
+      },
+    ],
+  },
+]);
